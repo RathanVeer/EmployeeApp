d2d127700b34deb9f4bd0003709cbe72749092ff
RathanVeer <rathanveer.balla@gmail.com>
2022-03-17 15:30:32 +0530

    added successfully

---
+++ src/com/mysql/jdbc/Statement.java
@@ -0,0 +1,12 @@
+package com.mysql.jdbc;
+
+import java.sql.ResultSet;
+
+public class Statement {
+
+	public ResultSet executeQuery(String q1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
